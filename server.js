03d3fdc5c6dafcc1fa1159ad06775b10e0af7b76
Eunbee Kim <ekim105@myseneca.ca>
2019-09-19 19:04:32 -0400

    update html & path in server.js file

--- server.js
+++ server.js
@@ -9,27 +9,27 @@ var HTTP_PORT = process.env.PORT || 8080;
 app.use(express.static("views"));
 app.use(express.static("data"));
 
-// call this function after the http server starts listening for requests
+
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-// setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "/views/index.html"));
     
 });
 
-// setup another route to listen on /about
-// app.get("/about", function(req,res){
-//   res.sendFile(path.join(__dirname, "/views/about.html"));
-// });
+app.get("/about", function(req,res){
+  res.sendFile(path.join(__dirname, "/views/about.html"));
+});
 
+app.get("/interest", function(req,res){
+  res.sendFile(path.join(__dirname, "/views/interest.html"));
+});
 
-// app.use(function(req, res) {
-//   res.status(404).sendFile(path.join(__dirname, "/views/error.html"));
-//   res.status(400).sendFile(path.join(__dirname, "/views/error.html"));
-// });
+app.use(function(req, res) {
+  res.status(404).sendFile(path.join(__dirname, "/views/error.html"));
+  res.status(400).sendFile(path.join(__dirname, "/views/error.html"));
+});
 
-// setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
