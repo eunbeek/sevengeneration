8b193372b8354d417ecceae515bf2ce96b652f69
Eunbee Kim <ekim105@myseneca.ca>
2019-09-25 17:06:23 -0400

    upload web main project

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get("/interest", function(req,res){
   res.sendFile(path.join(__dirname, "/views/interest.html"));
 });
 
+app.get("/webfront", function(req,res){
+  res.sendFile(path.join(__dirname, "/views/webfront.html"));
+});
+
 app.use(function(req, res) {
   res.status(404).sendFile(path.join(__dirname, "/views/error.html"));
   res.status(400).sendFile(path.join(__dirname, "/views/error.html"));
